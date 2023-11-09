79ee4e5c61b709f40d016c9d9ddee6dc3298ba89
Christopher Yu <christophergin88@gmail.com>
2023-11-09 21:22:21 +0800

    feat: new sections(skills and contact)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,8 @@ export default {
 			main: '#232946',
 			secondary: '#d4d8f0',
 			white: '#fffffe',
-			pink: '#eebbc3'
+			pink: '#eebbc3',
+			bluegray: '#b8c1ec'
 		},
 		extend: {},
 	},
@@ -28,6 +29,42 @@ export default {
 						rgba(238,187,200,0.1),
 						rgba(238,187,200,0.7) 4%,
 						rgba(238,187,200,0.3)
+					)`,
+					boxDecorationBreak: 'clone'
+				},
+				'.marker-effect-skills': {
+					position: 'absolute',
+					// content: '',
+					height: '70px',
+					zIndex: -1,
+					transform: 'rotate(-7deg)',
+					width: '185px',
+					margin: '0 -0.4em',
+					padding: '0.1em 0.4em',
+					borderRadius: '0.8em 0.3em',
+					background: 'transparent',
+					backgroundImage: `linear-gradient(to right,
+						rgba(255,255,254,0.1),
+						rgba(255,255,254,0.7) 4%,
+						rgba(255,255,254,0.3)
+					)`,
+					boxDecorationBreak: 'clone'
+				},
+				'.marker-effect-contact': {
+					position: 'absolute',
+					// content: '',
+					height: '70px',
+					zIndex: -1,
+					transform: 'rotate(-4deg)',
+					width: '410px',
+					margin: '0 -0.4em',
+					padding: '0.1em 0.4em',
+					borderRadius: '0.8em 0.3em',
+					background: 'transparent',
+					backgroundImage: `linear-gradient(to right,
+						rgba(212,216,240,0.1),
+						rgba(212,216,240,0.7) 4%,
+						rgba(212,216,240,0.3)
 					)`,
 					boxDecorationBreak: 'clone'
 				},
